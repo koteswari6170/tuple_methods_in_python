b2244424d221a5899a6bda29073e04f957b90123
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-04-16 19:03:09 +0530

    Add files via upload

---
+++ day_19.py
@@ -0,0 +1,37 @@
+# a="akhila",
+# print(a,type(a))
+
+# s=(2,5.6,'akhila',None)
+# print(s,type(s))
+# print(s[2])
+
+# a=print('hi')
+# b=print(a)
+# print(a,type(a))
+
+# a=(3)
+# print(a,type(a))
+
+# a=(2,)
+# print(a,type(a))
+
+# pr=('apple','samsung','asus','dell')
+# pr[1]="motorolla"
+
+# a="akhila"     ,      "reddy"
+# print(a,type(a))
+# print(a[1])
+# print(a[2])
+
+# c=(1,2.3,"akhi",[4,5],
+# ('A','H'),None,True)
+# print(c,type(c))
+
+# a=(1,2.3,"akhila",[2,6],('A','H'))
+# a[0]=21
+
+# a=(1,2.3,"akhila",[2,7],('A','H'))
+# print(a.count(2.3))
+# print(a.index([26,7]))
+
+print(dir(tuple))
